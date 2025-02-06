6a9b99b52b00cd7b5f827f5605611c6c8af10f54
Hyunmin Park <753429@student.dusd.net>
2025-02-06 14:21:08 -0800

    static
    done

---
+++ CreatingScriptsUnity/Assets/Scripts/Utilities.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities 
+{
+    public static int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+    // Start is called before the first frame update
+  
+}
